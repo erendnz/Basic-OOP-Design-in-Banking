8acf74a7d317311013444504d6a9f09c0590e3e7
Eren Deniz Kılıç <109575103+erendnz@users.noreply.github.com>
2024-05-14 21:12:09 +0300

    Add files via upload

---
+++ Bank/Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using Bank;
+
+void VeriGoster<T>(List<T> items)
+{
+    Console.WriteLine();
+    foreach (T item in items)
+        Console.WriteLine(item);
+}
+
+Banka banka = new Banka();
+
+VeriGoster(banka.Musteriler.Listele());
+
+VeriGoster(banka.Personeller.Listele());
+
+VeriGoster(banka.Vezneler.Listele());
+
+Console.WriteLine("-----------");
+//banka.MusaitVeznelereMusteriAta();
+VeriGoster(banka.Vezneler.Listele());
+VeriGoster(banka.Musteriler.Listele());
+
+Console.WriteLine("-----------");
+banka.Vezneler.IslemYap();
+
+VeriGoster(banka.Vezneler.Listele());
+VeriGoster(banka.Musteriler.Listele());
+
